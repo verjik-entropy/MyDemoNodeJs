9bf480c93537fc29d1375c195aab3d37628eea85
verjik-entropy <stanislav.verjikovskiy@entropyspot.com>
2022-02-16 18:40:15 +0200

    added karma and jasmine with some dummy tests

---
+++ test/index.controller.js
@@ -0,0 +1,9 @@
+describe('index controller test', () => {
+    it('true must be true', () => {
+        expect(true).toBeTrue
+    })
+
+    it('false to be false', () => {
+        expect(false).toBeFalse
+    })
+})
