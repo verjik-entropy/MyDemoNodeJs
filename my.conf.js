d171510cca8d014e7cd8fc264cf3a2d4cce4fc54
verjik-entropy <stanislav.verjikovskiy@entropyspot.com>
2022-02-16 20:18:40 +0200

    changed back index endpoint response

--- my.conf.js
+++ my.conf.js
@@ -12,7 +12,6 @@ module.exports = function(config) {
     // available frameworks: https://www.npmjs.com/search?q=keywords:karma-adapter
     frameworks: ['jasmine'],
 
-
     // list of files / patterns to load in the browser
     files: [
       'test/**/*.js'
