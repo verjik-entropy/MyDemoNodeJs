074aa8c5fe9fd1781b9be6001cca9ed182828a3c
verjik-entropy <stanislav.verjikovskiy@entropyspot.com>
2022-02-17 11:12:54 +0200

    added testspec.yml and changed hello world response to return version number

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World Back again!')
+  res.send('Hello World Back again! Version: 1')
 })
 
 app.listen(port, () => {
